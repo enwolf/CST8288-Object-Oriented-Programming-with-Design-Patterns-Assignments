79400c3440ea27996ca6e3b2c5fd4705c6beb75e
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-09 16:28:45 -0400

    Updated Class File ShortCodeGenerator.java
    This class is designed based on the Singleton Design Pattern.
    
    	- Added private static ShortCodeGenerator shortCodeGenerator;
    
    	  This is the variable that will hold the reference to the
    	  Singleton shortCodeGenerator object.
    
    	- Added private static boolean initialized.
    
    	  This variable is initialize to false, once the only instance of
    	  the shortCodeGenerator object is created this will be set to true.
    
        - Added method public static synchronized ShortCodeGenerator
    	  getInstance()
    
          This method is synchronized so that it is thread safe.
    
          This is the method used to access our Singleton
          shortCodeGenerator object by other classes, and ensures that
          only one instance can ever be generated.
    
          When called, this method will check to see if initialized = true
          if it does it will return the initialized object.
    
          If the shortCodeGenerator object has not been initialized, then it
          will create the single instance of this object and then return it.
    
        - Moved methods shortCode() and randomChars() from Log.java into
          this class.

--- Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/ShortCodeGenerator.java
+++ Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/ShortCodeGenerator.java
@@ -1,5 +1,44 @@
 package com.algonquin.loggy;
 
+import java.util.Random;
+
 public class ShortCodeGenerator {
+
+	private static ShortCodeGenerator shortCodeGenerator;
+
+	private static boolean initialized = false;
+
+	private ShortCodeGenerator() {
+
+	}
+
+	public static synchronized ShortCodeGenerator getInstance() {
+
+		if (initialized)
+			return shortCodeGenerator;
+
+		shortCodeGenerator = new ShortCodeGenerator();
+		initialized = true;
+
+		return shortCodeGenerator;
+
+	}
+
+	String shortCode() {
+
+		return randomChars(3) + "-" + randomChars(3) + "-" + randomChars(3);
+	}
+
+	private String randomChars(int n) {
+
+		String randomChars = "";
+		String chars = "abcdefghijklmnopqrstuvwxyz1234567890";
+		Random rnd = new Random();
+
+		for (int i = 0; i < n; i++)
+			randomChars += chars.charAt(rnd.nextInt(chars.length()));
+
+		return randomChars;
+	}
 
 }
