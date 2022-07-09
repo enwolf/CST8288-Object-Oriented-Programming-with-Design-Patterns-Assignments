1438a355f9be2358fa98e3afd504ce265dcf6f8d
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-09 18:30:26 -0400

    Updated Log Architect.java
    - Added construction methods for various logs.

--- Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/LogArchitect.java
+++ Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/LogArchitect.java
@@ -5,4 +5,20 @@ public class LogArchitect {
 	public LogArchitect() {
 
 	}
+
+	public void buildAudioLog(LogBuilder logBuilder) {
+
+	}
+
+	public void buildPhotoLog(LogBuilder logBuilder) {
+
+	}
+
+	public void buildVideoLog(LogBuilder logBuilder) {
+
+	}
+
+	public void buildTextLog(LogBuilder logBuilder) {
+
+	}
 }
