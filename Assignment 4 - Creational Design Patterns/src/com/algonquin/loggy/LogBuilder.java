b446a7b4c44d9ad0d1bc237290a3dc2ed72c92d6
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-09 17:23:26 -0400

    Updated LogBuilder.java
    Added variables to hold values to be inserted into the created log.
    
    Added default constructor.
    
    Since this class will never have its own values set the default
    empty constructor is all that is required, as the only job of this class
    will be to build objects of other classes.

--- Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/LogBuilder.java
+++ Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/LogBuilder.java
@@ -1,5 +1,18 @@
 package com.algonquin.loggy;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class LogBuilder {
+
+	private UUID uuid;
+	private String code;
+	private String name;
+	private String description;
+	private Date date;
+
+	public LogBuilder() {
+
+	}
 
 }
