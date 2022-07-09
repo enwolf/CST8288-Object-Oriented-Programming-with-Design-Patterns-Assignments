9793204488f5e1af3008e028e305b6aee06e0bbe
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-09 17:45:37 -0400

    Updated LogBuilder.java File
    	- Added setter methods for all parameters of Log
    
    	- Added CreateLog() method which returns the created log
    	  object.

--- Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/LogBuilder.java
+++ Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/LogBuilder.java
@@ -6,13 +6,67 @@ import java.util.UUID;
 public class LogBuilder {
 
 	private UUID uuid;
-	private String code;
+	private String shortCode;
 	private String name;
 	private String description;
 	private Date date;
+	private File attachment;
 
 	public LogBuilder() {
 
+	}
+
+	public LogBuilder setUUID(UUID uuid) {
+
+		this.uuid = uuid;
+
+		return this;
+
+	}
+
+	public LogBuilder setshortCode(String shortCode) {
+
+		this.shortCode = shortCode;
+
+		return this;
+
+	}
+
+	public LogBuilder setName(String name) {
+
+		this.name = name;
+
+		return this;
+
+	}
+
+	public LogBuilder setDescription(String description) {
+
+		this.description = description;
+
+		return this;
+
+	}
+
+	public LogBuilder setDate(Date date) {
+
+		this.date = date;
+
+		return this;
+
+	}
+
+	public LogBuilder setFile(File attachment) {
+
+		this.attachment = attachment;
+
+		return this;
+
+	}
+
+	public Log createLog() {
+
+		return new Log(uuid, shortCode, name, description, date, attachment);
 	}
 
 }
