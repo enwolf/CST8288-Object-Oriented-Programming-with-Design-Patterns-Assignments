b61e36508f548f2e060d8a97e4367f94deb94f32
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-09 23:26:43 -0400

    Updated File Main.java
    	- Re-added log.attachFile(file) call in mockLog method.
    	- Re-formated LogBuilder.CreateLog() commands so that file is
    	  attached correctly for validation.

--- Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/Main.java
+++ Assignment 4 - Creational Design Patterns/src/com/algonquin/loggy/Main.java
@@ -8,8 +8,8 @@ public class Main {
 		try {
 			// Mock-up logs with non-supported content type
 			mockLog(logBuilder.setUUID().setShortCode().setName("first log")
-					.setDescription("Monday May 3, I had to wake up early").setDate()
-					.setFile(new ImageFile("image.tif", "TIF", "******", Long.valueOf(1024))).createImageLog());
+					.setDescription("Monday May 3, I had to wake up early").setDate().setFile(null).createImageLog(),
+					new ImageFile("image.tif", "TIF", "******", Long.valueOf(1024)));
 		} catch (Exception e) {
 			System.out.println("Exception: " + e.getMessage() + "\n\n");
 
@@ -18,8 +18,8 @@ public class Main {
 		try {
 			// Mock-up logs with incorrect attachment
 			mockLog(logBuilder.setUUID().setShortCode().setName("fourth log")
-					.setDescription("And even later, I need to have lunch").setDate()
-					.setFile(new TextFile("eating.png", "PNG", "******", Long.valueOf(1024))).createTextLog());
+					.setDescription("And even later, I need to have lunch").setDate().setFile(null).createTextLog(),
+					new TextFile("eating.png", "PNG", "******", Long.valueOf(1024)));
 		} catch (Exception e) {
 			System.out.println("Exception: " + e.getMessage() + "\n\n");
 
@@ -28,19 +28,19 @@ public class Main {
 		try {
 			// Mock-up logs with correct attachment
 			mockLog(logBuilder.setUUID().setShortCode().setName("first log")
-					.setDescription("Monday May 4, I had to wake up early").setDate()
-					.setFile(new ImageFile("image.png", "PNG", "******", Long.valueOf(1024))).createImageLog());
+					.setDescription("Monday May 4, I had to wake up early").setDate().setFile(null).createImageLog(),
+					new ImageFile("image.png", "PNG", "******", Long.valueOf(1024)));
 			mockLog(logBuilder.setUUID().setShortCode().setName("second log")
-					.setDescription("Few minutes later, I had my first cup of coffee").setDate()
-					.setFile(new ImageFile("coffee.png", "PNG", "******", Long.valueOf(1024))).createImageLog());
+					.setDescription("Few minutes later, I had my first cup of coffee").setDate().setFile(null)
+					.createImageLog(), new ImageFile("coffee.png", "PNG", "******", Long.valueOf(1024)));
 
 			mockLog(logBuilder.setUUID().setShortCode().setName("third log")
-					.setDescription("Few minutes later, I am going for a ride").setDate()
-					.setFile(new VideoFile("biking1.mp4", "MP4", "******", Long.valueOf(1024))).createVideoLog());
+					.setDescription("Few minutes later, I am going for a ride").setDate().setFile(null)
+					.createVideoLog(), new VideoFile("biking1.mp4", "MP4", "******", Long.valueOf(1024)));
 
 			mockLog(logBuilder.setUUID().setShortCode().setName("fourth log")
-					.setDescription("And even later, I need to have lunch").setDate()
-					.setFile(new TextFile("recipie.txt", "TXT", "******", Long.valueOf(1024))).createTextLog());
+					.setDescription("And even later, I need to have lunch").setDate().setFile(null).createTextLog(),
+					new TextFile("recipie.txt", "TXT", "******", Long.valueOf(1024)));
 		} catch (Exception e) {
 			System.out.println("Exception: " + e.getMessage() + "\n\n");
 
@@ -48,8 +48,9 @@ public class Main {
 
 	}
 
-	private static void mockLog(Log log) throws Exception {
+	private static void mockLog(Log log, File file) throws Exception {
 		System.out.println(">>> New log (" + log.toString() + ")");
+		log.attachFile(file);
 		System.out.println("\n");
 	}
 }
