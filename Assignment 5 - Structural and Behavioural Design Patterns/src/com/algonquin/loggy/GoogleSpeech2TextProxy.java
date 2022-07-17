06b392e692e809e63f304a1370fc188c9f6f160d
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-17 19:59:28 -0400

    Updated File GoogleSpeech2TextProxy
    Added new methods:
    
    	- instantiateClient()
    
    	- fileToMemory()
    
    	- buildSyncRecognizeRequestConfig()
    
    	- buildSyncRecognizeRequestAudio()
    
    	- performSpeechRecognition()
    
    	- getFirstTranscriptAlternative()

--- Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/GoogleSpeech2TextProxy.java
+++ Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/GoogleSpeech2TextProxy.java
@@ -1,5 +1,33 @@
 package com.algonquin.loggy;
 
+import com.google.api.GoogleSpeech2TextAPI;
+
 public class GoogleSpeech2TextProxy {
+
+	private GoogleSpeech2TextAPI googleApiObj = new GoogleSpeech2TextAPI();
+
+	public String instantiateClient() {
+		return googleApiObj.instantiateClient();
+	}
+
+	public String fileToMemory(String fileName) {
+		return googleApiObj.fileToMemory(fileName);
+	}
+
+	public String buildSyncRecognizeRequestConfig() {
+		return googleApiObj.buildSyncRecognizeRequestConfig();
+	}
+
+	public String buildSyncRecognizeRequestAudio() {
+		return googleApiObj.buildSyncRecognizeRequestAudio();
+	}
+
+	public void performSpeechRecognition(String config, String audio) {
+		googleApiObj.performSpeechRecognition(config, audio);
+	}
+
+	public String getFirstTranscriptAlternative() {
+		return googleApiObj.getFirstTranscriptAlternative();
+	}
 
 }
