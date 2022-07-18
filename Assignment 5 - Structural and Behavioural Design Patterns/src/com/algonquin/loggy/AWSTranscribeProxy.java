36fabd559edf5a74665c77edbee7e316a7498aae
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-17 20:16:21 -0400

    Updated File AWSTranscribeProxy.java
    Added private object of type AWSTranscribeAPI to us within the proxy
    methods.
    
    Added methods:
    
    	- clientCreate()
    
    	- getStreamFromFile()
    
    	- startStreamTranscription()
    
    	- getResult()
    
    	- clientClose()

--- Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/AWSTranscribeProxy.java
+++ Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/AWSTranscribeProxy.java
@@ -1,5 +1,29 @@
 package com.algonquin.loggy;
 
+import com.aws.api.AWSTranscribeAPI;
+
 public class AWSTranscribeProxy {
+
+	private AWSTranscribeAPI AWSProxyObj = new AWSTranscribeAPI();
+
+	public String clientCreate() {
+		return AWSProxyObj.clientCreate();
+	}
+
+	public String getStreamFromFile(String audioFileName) {
+		return AWSProxyObj.getStreamFromFile(audioFileName);
+	}
+
+	public void startStreamTranscription(String client, String stream) {
+		AWSProxyObj.startStreamTranscription(client, stream);
+	}
+
+	public String getResult() {
+		return AWSProxyObj.getResult();
+	}
+
+	public void clientClose(String client) {
+		AWSProxyObj.clientClose(client);
+	}
 
 }
