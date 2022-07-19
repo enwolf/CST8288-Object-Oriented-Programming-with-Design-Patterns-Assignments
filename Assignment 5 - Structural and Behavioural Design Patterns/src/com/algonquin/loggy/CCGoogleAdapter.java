8905b080d8fc12f411a38266d1fbb2282d781c86
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-19 15:45:22 -0400

    Updated Class File: CCGoogleAdapter.java
    Added default constructor.
    
    Implemented ICCAdapter interface
    
    Implemented interface methods
    
    	- public void triggerClosedCaptioning(Recording recording)
    
    Copied close caption process from CCWorker.java into our adapter method.

--- Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/CCGoogleAdapter.java
+++ Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/CCGoogleAdapter.java
@@ -1,5 +1,37 @@
 package com.algonquin.loggy;
 
-public class CCGoogleAdapter {
+public class CCGoogleAdapter implements ICCAdapter {
+
+	public CCGoogleAdapter() {
+
+	}
+
+	@Override
+	public void triggerClosedCaptioning(Recording recording) {
+
+		GoogleSpeech2TextProxy googleProxyAPI = new GoogleSpeech2TextProxy();
+
+		String ccFile = "";
+		Long fileSize = recording.getFileSize();
+		String rawFile = recording.getMediaFileMock();
+
+		try {
+			// Simulate the delay.
+			Thread.sleep(fileSize);
+			// MockUp transcript process.
+			String speechClient = googleProxyAPI.instantiateClient();
+			String audioBytes = googleProxyAPI.fileToMemory(rawFile);
+			String config = googleProxyAPI.buildSyncRecognizeRequestConfig();
+			String audio = googleProxyAPI.buildSyncRecognizeRequestAudio();
+			googleProxyAPI.performSpeechRecognition(config, audio);
+			String transcript = googleProxyAPI.getFirstTranscriptAlternative();
+			ccFile = transcript;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		recording.setCcFileMock(ccFile);
+		System.out.println(ccFile + " processed using GoogleSpeech2TextAPI");
+
+	}
 
 }
