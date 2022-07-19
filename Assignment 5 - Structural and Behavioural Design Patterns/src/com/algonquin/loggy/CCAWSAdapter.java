9107a739d7461a1c761e847c6226910e8438007a
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-19 15:41:12 -0400

    Updated Class File: CCAWSAdapter.java
    Implemented  ICCAdapter interface
    
    Implemented interface methods
    
    	- public void triggerClosedCaptioning(Recording recording)
    
    Copied close caption process from CCWorker.java into our adapter method.

--- Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/CCAWSAdapter.java
+++ Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/CCAWSAdapter.java
@@ -1,5 +1,34 @@
 package com.algonquin.loggy;
 
-public class CCAWSAdapter {
+public class CCAWSAdapter implements ICCAdapter {
+
+	@Override
+	public void triggerClosedCaptioning(Recording recording) {
+
+		AWSTranscribeProxy AWSProxyApi = new AWSTranscribeProxy();
+
+		String ccFile = "";
+		Long fileSize = recording.getFileSize();
+		String rawFile = recording.getMediaFileMock();
+
+		System.out.println("Closed captioning " + rawFile + " will take " + fileSize + " milliseconds...");
+
+		try {
+			// Simulate the delay.
+			Thread.sleep(fileSize);
+			// MockUp transcript process.
+			String client = AWSProxyApi.clientCreate();
+			String stream = AWSProxyApi.getStreamFromFile(rawFile);
+			AWSProxyApi.startStreamTranscription(client, stream);
+			String transcript = AWSProxyApi.getResult();
+			AWSProxyApi.clientClose(client);
+			ccFile = transcript;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		recording.setCcFileMock(ccFile);
+		System.out.println(ccFile + " processed using AWSTranscribeAPI");
+
+	}
 
 }
