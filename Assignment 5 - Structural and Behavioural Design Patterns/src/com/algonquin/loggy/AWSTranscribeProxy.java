b49f5d97b0707b66b30fe2d4a8d04ef8cf990c02
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-19 15:30:18 -0400

    Updated Class File: AWSTranscribeProxy.java
    Implemented IAWSTranscribeAPI interface
    
    Added @Override annotation for interface methods

--- Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/AWSTranscribeProxy.java
+++ Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/AWSTranscribeProxy.java
@@ -2,26 +2,31 @@ package com.algonquin.loggy;
 
 import com.aws.api.AWSTranscribeAPI;
 
-public class AWSTranscribeProxy {
+public class AWSTranscribeProxy implements IAWSTranscribeAPI {
 
 	private AWSTranscribeAPI AWSProxyObj = new AWSTranscribeAPI();
 
+	@Override
 	public String clientCreate() {
 		return AWSProxyObj.clientCreate();
 	}
 
+	@Override
 	public String getStreamFromFile(String audioFileName) {
 		return AWSProxyObj.getStreamFromFile(audioFileName);
 	}
 
+	@Override
 	public void startStreamTranscription(String client, String stream) {
 		AWSProxyObj.startStreamTranscription(client, stream);
 	}
 
+	@Override
 	public String getResult() {
 		return AWSProxyObj.getResult();
 	}
 
+	@Override
 	public void clientClose(String client) {
 		AWSProxyObj.clientClose(client);
 	}
