ad882d5fed877beca423bde3e07a32d05b637478
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-19 15:18:02 -0400

    Updated Interface File IAWSTranscribeAPI.java
    Added methods:
    
    	- public String getStreamFromFile(String audioFileName);
    
    	- public void startStreamTranscription(String client, String stream);
    
    	- public String getResult();
    
    	- public String clientCreate();
    
    	- public void clientClose(String client);

--- Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/IAWSTranscribeAPI.java
+++ Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/IAWSTranscribeAPI.java
@@ -2,4 +2,13 @@ package com.algonquin.loggy;
 
 public interface IAWSTranscribeAPI {
 
+	public String getStreamFromFile(String audioFileName);
+
+	public void startStreamTranscription(String client, String stream);
+
+	public String getResult();
+
+	public String clientCreate();
+
+	public void clientClose(String client);
 }
