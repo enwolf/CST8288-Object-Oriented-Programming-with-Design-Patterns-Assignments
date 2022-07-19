36fbf9bab2129fd796e64cee10f6ef5cbae9ff36
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-19 15:31:44 -0400

    Updated Class File: GoogleSpeech2TextProxy
    Implemented IGoogleSpeech2TextAPI interface
    
    Added unimplemented interface methods
    
    	- public List<String> getResultList();
    
    	- public String recognitionAudio();
    
    	- public void setRecognitionConfigParameters();
    
    Added @Override annotation for interface methods

--- Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/GoogleSpeech2TextProxy.java
+++ Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/GoogleSpeech2TextProxy.java
@@ -1,33 +1,58 @@
 package com.algonquin.loggy;
 
+import java.util.List;
+
 import com.google.api.GoogleSpeech2TextAPI;
 
-public class GoogleSpeech2TextProxy {
+public class GoogleSpeech2TextProxy implements IGoogleSpeech2TextAPI {
 
 	private GoogleSpeech2TextAPI googleApiObj = new GoogleSpeech2TextAPI();
 
+	@Override
 	public String instantiateClient() {
 		return googleApiObj.instantiateClient();
 	}
 
+	@Override
 	public String fileToMemory(String fileName) {
 		return googleApiObj.fileToMemory(fileName);
 	}
 
+	@Override
 	public String buildSyncRecognizeRequestConfig() {
 		return googleApiObj.buildSyncRecognizeRequestConfig();
 	}
 
+	@Override
 	public String buildSyncRecognizeRequestAudio() {
 		return googleApiObj.buildSyncRecognizeRequestAudio();
 	}
 
+	@Override
 	public void performSpeechRecognition(String config, String audio) {
 		googleApiObj.performSpeechRecognition(config, audio);
 	}
 
+	@Override
 	public String getFirstTranscriptAlternative() {
 		return googleApiObj.getFirstTranscriptAlternative();
+	}
+
+	@Override
+	public List<String> getResultList() {
+		return googleApiObj.getResultList();
+	}
+
+	@Override
+	public String recognitionAudio() {
+
+		return googleApiObj.recognitionAudio();
+	}
+
+	@Override
+	public void setRecognitionConfigParameters() {
+		googleApiObj.setRecognitionConfigParameters();
+
 	}
 
 }
