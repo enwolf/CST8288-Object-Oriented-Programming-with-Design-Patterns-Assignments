acdaeb4431a3d8fa52174d2f6b28735f3b91583f
enwolf <35503014+enwolf@users.noreply.github.com>
2022-07-20 01:18:37 -0400

    Updated Class File: RecordingObserver.java
    Added extra variable being tracked on update() and display()

--- Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/RecordingObserver.java
+++ Assignment 5 - Structural and Behavioural Design Patterns/src/com/algonquin/loggy/RecordingObserver.java
@@ -5,6 +5,7 @@ public class RecordingObserver implements Observer {
 	private String fileName;
 	private String ccFile;
 	private Subject recording;
+	private Long fileSize;
 
 	public RecordingObserver(Subject recording) {
 		this.recording = recording;
@@ -12,16 +13,17 @@ public class RecordingObserver implements Observer {
 	}
 
 	@Override
-	public void update(String ccFile, String fileName) {
+	public void update(String ccFile, String fileName, Long fileSize) {
 		this.ccFile = ccFile;
 		this.fileName = fileName;
+		this.fileSize = fileSize;
 		display();
 	}
 
 	public void display() {
 
-		System.out.println(
-				"RecordingObserver Reporting - RecordingName: " + fileName + " has attched ccFile: " + ccFile + "\n");
+		System.out.println("RecordingObserver Reporting - RecordingName: " + fileName + " has attched ccFile: " + ccFile
+				+ " final file size on disk: " + fileSize + "\n");
 
 	}
 
